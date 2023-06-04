dbf3a1971f32f5219d4a431752ddc65c0d7b3a79
Karthik <33004854+Karthi1908@users.noreply.github.com>
2023-06-04 15:00:26 +0800

    added events

--- contracts/PrintObject.sol
+++ contracts/PrintObject.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.9;
 import '@openzeppelin/contracts/access/Ownable.sol';
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import './IPublicLock.sol';
 import './PrintApprovalToken.sol';
 import './PrintNearMeToken.sol';
@@ -11,7 +12,7 @@ import './PrintNearMeToken.sol';
 
 
 
-contract PrintObject is Ownable {
+contract PrintObject is Ownable, ReentrancyGuard {
 
     PrintApprovalToken public PAT;
     PrintNearMeToken public PNMT;
@@ -35,6 +36,20 @@ contract PrintObject is Ownable {
         PNMT = PrintNearMeToken(_PNMTAddress);
     }
 
+    event Withdrawls(
+        address indexed user,
+        string indexed currency,
+        uint256 amount        
+    );
+
+    event ObjectPrinted(
+        address indexed user,
+        uint indexed tokenId,
+        string indexed printerName,
+        uint256 amount,
+        string currency       
+    );
+
     function printObject( uint _tokenId, string memory _printerName, string memory _currency) public {
 
         if(PNMT.ownerOf(_tokenId) == msg.sender){
@@ -50,6 +65,7 @@ contract PrintObject is Ownable {
         }
 
         require(keccak256(abi.encodePacked((_currency))) ==keccak256(abi.encodePacked((printerList[_printerName].feeCurrency))) , "Currency needs to same as Listed Currency");
+        balances[printerList[_printerName].printOwner][_currency] += printerList[_printerName].cost ;
 
         if (keccak256(abi.encodePacked((_currency))) == keccak256(abi.encodePacked(('matic')))) {
 
@@ -60,6 +76,8 @@ contract PrintObject is Ownable {
             require(getBalanceOfToken(tokenList[_currency], msg.sender) > printerList[_printerName].cost,"Insufficient Balance");
             IERC20(tokenList[_currency]).transferFrom(msg.sender, address(this), printerList[_printerName].cost);
         }
+
+        emit ObjectPrinted(msg.sender,_tokenId, _printerName, printerList[_printerName].cost, _currency );
         
         
     }
@@ -83,6 +101,29 @@ contract PrintObject is Ownable {
 
         require(printerList[printerName].printOwner != address(0));
         require(printerList[printerName].status == 1);       
+
+    }
+
+    function withdrawProceeds(string memory _currency, uint _amount) external nonReentrant {
+
+        require(balances[msg.sender][_currency] >= _amount, "Not enough balance for User");
+        balances[msg.sender][_currency] -= _amount;
+
+        if (keccak256(abi.encodePacked((_currency))) == keccak256(abi.encodePacked(('matic')))) {
+
+            require(address(this).balance > _amount, "Not enough balance in contract");
+            
+            (bool success, ) = payable(msg.sender).call{value: _amount}("");
+            require(success, "Transfer failed");
+
+        } else {
+
+            require(getBalanceOfToken(tokenList[_currency], address(this)) > _amount, "Not enough balance in contract");
+            
+            IERC20(tokenList[_currency]).transfer(msg.sender, _amount);
+
+        }
+        emit Withdrawls(msg.sender , _currency, _amount);
 
     }
 
